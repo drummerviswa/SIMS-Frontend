121ce99221549bcc21dda89dfb96904b77c43c89
rajeshgit2004@gmail.com <rajeshgit2004@gmail.com>
2025-03-03 05:17:09 +0530

    Home Page Template

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,30 +1,9 @@
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "../slices/example.slice";
+import React from 'react'
 
-function Homepage() {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
+const Homepage = () => {
   return (
-    <div className="flex items-center justify-center h-screen gap-4">
-      <button
-        className="btn btn-success"
-        aria-label="Increment value"
-        onClick={() => dispatch(increment())}
-      >
-        Increment
-      </button>
-      <span className="text-6xl">
-        <span>{count}</span>
-      </span>
-      <button
-        className="btn btn-error"
-        aria-label="Decrement value"
-        onClick={() => dispatch(decrement())}
-      >
-        Decrement
-      </button>
-    </div>
-  );
+    <div>Homepage</div>
+  )
 }
 
-export default Homepage;
+export default Homepage
