afd098c861bd34a5ca3820949fb5e13b0425a48e
Viswanathan P <drummerviswa@gmail.com>
2025-06-23 15:18:22 +0530

    Feat:database

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import "simplebar-react/dist/simplebar.min.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <AppWrapper>
+        <Analytics />
         <App />
       </AppWrapper>
     </ThemeProvider>
